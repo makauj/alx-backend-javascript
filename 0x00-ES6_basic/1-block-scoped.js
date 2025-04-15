01a5027d0ec17d258c353f0e05b8739a2c4707fe
makau6 <makauwanyoike@gmail.com>
2025-04-15 18:33:03 +0300

    Eslint

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,8 +1,7 @@
-#!/user/bin/node
-
+/* eslint-disable no-unused-vars */
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
     const task = true;
