93789e87e373680d0acba7809ed423eb70aa3f31
makau6 <makauwanyoike@gmail.com>
2025-04-16 04:14:44 +0300

    better js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,9 +3,13 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   const photo = await uploadPhoto();
   const user = await createUser();
+  let response = [];
 
-  return {
-    photo: photo || null,
-    user: user || null,
-  };
+  try {
+    response = { photo, user };
+  } catch (error) {
+    response = { photo: null, user: null };
+  }
+
+  return response;
 }
