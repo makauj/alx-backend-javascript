d97b8196d2067ef886cfa895b6d5ce7f1940cabe
makau6 <makauwanyoike@gmail.com>
2025-04-16 04:21:06 +0300

    eslint again

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,7 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
