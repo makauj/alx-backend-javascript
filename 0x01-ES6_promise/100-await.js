d4947d48d2d902d28f952e85ab05d0fadbad3c59
makau6 <makauwanyoike@gmail.com>
2025-04-16 04:16:21 +0300

    why []

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   const photo = await uploadPhoto();
   const user = await createUser();
-  let response = [];
+  let response = {};
 
   try {
     response = { photo, user };
