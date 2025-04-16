0999e278633bfce36b9ebd456bc9a48dfb18b0df
makau6 <makauwanyoike@gmail.com>
2025-04-16 03:46:17 +0300

    final user

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import { signUpUser } from './4-user-promise';
+import { uploadPhoto } from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise.allSettled([user, photo]).then((values) => {
+    const outPut = [];
+    values.forEach((value) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: value.status, value: value.value });
+      } else {
+        result.push({ status: value.status, value: `$(value.reason)` });
+      }
+    });
+    return outPut;
+  });
+}
