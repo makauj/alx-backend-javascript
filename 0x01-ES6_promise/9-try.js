07ad893ea21fbb3c3f843b332ebfcc8f10af933a
makau6 <makauwanyoike@gmail.com>
2025-04-16 03:58:24 +0300

    8 & 9 try

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export function guardRail(mathFunction) {
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
