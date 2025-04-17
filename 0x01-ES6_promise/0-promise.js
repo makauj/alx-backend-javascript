b24d82b8e8906139602edb705336b472d6c05351
makau6 <makauwanyoike@gmail.com>
2025-04-17 22:34:11 +0300

    return promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,3 @@
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({
-        status: 200,
-        body: 'success'
-      });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
+  return new Promise(() => {});
 }
