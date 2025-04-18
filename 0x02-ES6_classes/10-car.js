d832c4f362c9f1f9ad8309a58d8aacbc02944f7a
makau6 <makauwanyoike@gmail.com>
2025-04-18 18:51:07 +0300

    bugs and eslint

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,10 +1,35 @@
 export default class Car {
- constructor(brand, motor, year) {
+  constructor(brand, motor, year) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
- }
- cloneCar() {
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  static cloneCar() {
     return new Car(this._brand, this._motor, this._color);
- }
+  }
 }
