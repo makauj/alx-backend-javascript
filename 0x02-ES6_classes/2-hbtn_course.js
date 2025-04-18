0605372ff049cc36a791879ca020d7bc487d2532
makau6 <makauwanyoike@gmail.com>
2025-04-18 03:56:15 +0300

    hbtn course

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,39 @@
+export default class ALXCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+  // getter methos
+  get name() {
+    return this._name;
+  }
+  get length() {
+    return this._length;
+  }
+  get students() {
+    return this._students;
+  }
+  //setter methods
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+  }
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+  }
+  set students(value) {
+    if (!Array.isArray(value)) {
+      throw new TypeError('students must be an array');
+    }
+    for (const student of value) {
+      if (typeof student !== 'string') {
+        throw new TypeError('Students must be an array of strings');
+      }
+    }
+    this._students = value;
+  }
+}
