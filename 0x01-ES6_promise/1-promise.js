beb2e4a12ce619b20833fe42deb3ccc4e49e94d2
makau6 <makauwanyoike@gmail.com>
2025-04-18 21:39:02 +0300

    promises

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
 function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success) {
+    if (success === true) {
       resolve({ status: 200, body: 'Success' });
     } else {
       reject(new Error('The fake API is not working currently'));
