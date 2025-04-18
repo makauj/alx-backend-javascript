7dcbaf07d843544f8b776d0d8611a76217b829d2
makau6 <makauwanyoike@gmail.com>
2025-04-18 19:07:46 +0300

    bug smashing

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -4,10 +4,19 @@ import Car from './10-car';
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this.range = range;
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
   }
 
   cloneCar() {
+    const Car = super.constructor[Symbol.species];
     return new Car();
   }
 }
