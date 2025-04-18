943dd08e74d71b05c96a461fbf71f7cd5dba455c
makau6 <makauwanyoike@gmail.com>
2025-04-18 04:07:58 +0300

    this statements

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -8,23 +8,30 @@ export default class ALXCourse {
   get name() {
     return this._name;
   }
+
   get length() {
     return this._length;
   }
+
   get students() {
     return this._students;
   }
+
   //setter methods
   set name(value) {
     if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
     }
+    this._name = value;
   }
+
   set length(value) {
     if (typeof value !== 'number') {
       throw new TypeError('Length must be a number');
     }
+    this._length = value;
   }
+
   set students(value) {
     if (!Array.isArray(value)) {
       throw new TypeError('students must be an array');
