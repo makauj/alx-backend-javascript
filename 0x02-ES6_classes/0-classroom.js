bac7065a5fa797baffbfaa64ab0f2ed5708208b8
makau6 <makauwanyoike@gmail.com>
2025-04-18 03:39:01 +0300

    max student size

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,8 +2,4 @@ export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
-
-  get maxStudentsSize() {
-    return this._maxStudentsSize;
-  }
 }
