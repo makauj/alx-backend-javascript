350cd93b9f4fe35c067dd2f8916932b927afd066
makau6 <makauwanyoike@gmail.com>
2025-04-23 18:13:15 +0300

    more weak

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,11 +3,10 @@ export const weakMap = new WeakMap();
 export function queryApi(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
-  } else {
-    const count = weakMap.get(endpoint) + 1;
-    weakMap.set(endpoint, count);
-    if (count >= 5) {
-      throw new Error('Endpoint load is high');
-    }
+  }
+  const count = weakMap.get(endpoint) + 1;
+  weakMap.set(endpoint, count);
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
   }
 }
