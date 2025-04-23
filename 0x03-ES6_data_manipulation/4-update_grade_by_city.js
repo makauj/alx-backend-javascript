6156af0cb288941fa8b8b41679313c383136230c
makau6 <makauwanyoike@gmail.com>
2025-04-23 20:39:50 +0300

    more filters

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,16 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   const studentsInCity = students.filter((student) => student.location === city);
   return studentsInCity.map((student) => {
-    const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+    const gradeObj = newGrades.filter((grade) => grade.studentId === student.id);
+    if (gradeObj.length === 0) {
+      return {
+      ...student,
+      grade: gradeObj[0].grade,
+      };
+    }
     return {
       ...student,
-      grade: gradeObj ? gradeObj.grade : 'N/A',
+      grade: 'N/A',
     };
   });
 }
