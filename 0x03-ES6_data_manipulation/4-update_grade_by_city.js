4815a5167dda8af6a8749537c26ff59028600457
makau6 <makauwanyoike@gmail.com>
2025-04-23 20:42:12 +0300

    update code

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   const studentsInCity = students.filter((student) => student.location === city);
   return studentsInCity.map((student) => {
     const gradeObj = newGrades.filter((grade) => grade.studentId === student.id);
-    if (gradeObj.length === 0) {
+    if (gradeObj.length > 0) {
       return {
       ...student,
       grade: gradeObj[0].grade,
