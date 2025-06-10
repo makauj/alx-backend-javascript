3ad8bcd4a9ba56b2427090dbd1a9842853a1dba4
makau6 <makauwanyoike@gmail.com>
2025-06-10 20:16:32 +0300

    refactor: remove shebang and comments, update response message in 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,8 @@
-#!/usr/bin/node
-// samll http server using http module
-// variable 'app' must be exported
-// port 1245
 const http = require('http');
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello ALX!\n');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
