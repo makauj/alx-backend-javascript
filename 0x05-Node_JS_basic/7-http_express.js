25dccd492741e0869922bdcc61b95f58da03959e
makau6 <makauwanyoike@gmail.com>
2025-06-10 20:56:59 +0300

    refactor: correct variable name and update greeting message in 7-http_express.js

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -2,20 +2,20 @@ const express = require('express');
 const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
-const DATABASE = args[0];
+const DB = args[0];
 const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
   res.set('Content-Type', 'text/plain');
-  res.send('Hello ALX!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
   res.set('Content-Type', 'text/plain');
   const msg = 'This is the list of our students\n';
   try {
-    const students = await countStudents(DATABASE); 
+    const students = await countStudents(DB); 
     res.send(`${msg}${students.join('\n')}`);
   } catch (error) {
     res.send(`${msg}${error.message}`);
