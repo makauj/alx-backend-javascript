3fad5cecf3c32030f57b9df8d48df43e8373a22a
makau6 <makauwanyoike@gmail.com>
2025-06-10 04:31:40 +0300

    initialize Express server with basic route and listener

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const port = 1245;
+app.get('/', (req, res) => {
+  res.send('Hello ALX!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+module.exports = app;
